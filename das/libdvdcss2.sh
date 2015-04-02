64c3467b090687875c9be27c45e7517458ca056e
root <root@debian.lan>
2015-04-02 16:04:10 +0200

    libdvdcss2, magnet, youtube-dl working

--- das/libdvdcss2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp /etc/apt/sources.list /etc/apt/sources.list.bak
-sed -i -e '/multimedia/ s/#//' /etc/apt/sources.list
-apt-get update
-apt-get install libdvdcss2
-cp /etc/apt/sources.list.bak /etc/apt/sources.list
-apt-get update
