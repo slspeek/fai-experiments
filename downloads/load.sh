ba251bc574f09c21f5a36f10c2e9b2b0d6e288e6
root <root@debian.lan>
2015-04-24 14:27:30 +0200

    Download cache, synaptic via gksu

--- downloads/load.sh
+++ downloads/load.sh
@@ -1,1 +1,9 @@
-wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2014.2_all.deb
+wget -c http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2014.2_all.deb
+
+VERSION=1.4.2
+ARCH=386
+wget -c https://storage.googleapis.com/golang/go${VERSION}.linux-${ARCH}.tar.gz
+
+ARCH=amd64
+wget -c https://storage.googleapis.com/golang/go${VERSION}.linux-${ARCH}.tar.gz
+
