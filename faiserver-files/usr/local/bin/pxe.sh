a78ea312b2aeb3a66be53704bdb677fd932363ca
root <root@faijes>
2015-04-30 12:01:50 +0200

    SALE jessie has CUPS

--- faiserver-files/usr/local/bin/pxe.sh
+++ faiserver-files/usr/local/bin/pxe.sh
@@ -2,5 +2,5 @@
 
 HOST=$1
 
-PXECONFIG=$(fai-chboot -u nfs://10.0.40.2/srv/fai/config -IFv $HOST 2>&1|tail -1|cut -d' ' -f3)
-sed -i -e 's%/srv/fai/nfsroot%10.0.40.2:/srv/fai/nfsroot:vers=3%' $PXECONFIG
+PXECONFIG=$(fai-chboot -u nfs://10.0.50.2/srv/fai/config -IFv $HOST 2>&1|tail -1|cut -d' ' -f3)
+#sed -i -e 's%/srv/fai/nfsroot%10.0.40.2:/srv/fai/nfsroot:vers=3%' $PXECONFIG
