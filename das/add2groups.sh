46c9c20d651c8e7ac683417844710c1c16267aaf
root <root@faijes>
2015-05-12 13:53:49 +0200

    USERTUX, DESCRIBES, DEBIANAMS classes

--- das/add2groups.sh
+++ das/add2groups.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -x
 USER=${1:-tux}
-for GROUP in audio video cdrom plugdev floppy sudo netdev scanner fuse lpadmin
+for GROUP in audio video cdrom plugdev dip floppy sudo netdev scanner fuse bluetooth lpadmin
 do
   adduser $USER $GROUP
 done
