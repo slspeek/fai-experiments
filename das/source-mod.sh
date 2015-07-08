b73e326feddf1d4d8faba5906a112d430db4b0a1
root <root@fai32>
2015-07-08 15:21:11 +0200

    Lokale bronnen JD

--- das/source-mod.sh
+++ das/source-mod.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 WGET="wget --timeout 10 --tries 1"
-# For Postjeskade
-if $WGET http://10.0.0.112/debian; then
-  sed -i -e '/10.0.0.112/ s/^#//' /etc/apt/sources.list
-  apt-get update
-fi
 # For Zeeburgerpad
 if $WGET http://10.0.20.181/debian; then
   sed -i -e '/10.0.20.181/ s/^#//' /etc/apt/sources.list
