2c9be0a2951c19c934e132dbccaee0fa3b4c9153
root <root@faijes>
2015-07-08 16:41:31 +0200

    FAI set

--- das/install-packages.sh
+++ das/install-packages.sh
@@ -2,9 +2,8 @@
 
 
 TMPFILE=$(mktemp)
-
-fai-class /srv/fai/config/class $TMPFILE
-
 mkdir -p /tmp/fai
 touch /tmp/fai/status.log
-classes=$(cat $TMPFILE) FAI_ROOT=/ install_packages
+
+fai-class /srv/fai/config/class $TMPFILE
+FAI=/srv/fai/config classes=$(cat $TMPFILE) FAI_ROOT=/ install_packages
