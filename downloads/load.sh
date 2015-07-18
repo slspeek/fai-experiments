b4eee6897c49287dfb257bc9496540141b7ab7ae
root <root@flysuit.lan>
2015-07-19 00:02:34 +0200

    faiserver-files for 1-interface servers, Popcorntime 0.3.8-0

--- downloads/load.sh
+++ downloads/load.sh
@@ -8,14 +8,14 @@ ARCH=amd64
 wget -c https://storage.googleapis.com/golang/go${VERSION}.linux-${ARCH}.tar.gz
 
 
-VERSION=0.3.7.2
+VERSION=0.3.8-0
 
-ARCH=Linux32
+ARCH=Linux-32
 ARCHIVE=Popcorn-Time-${VERSION}-${ARCH}.tar.xz
 URLBASE=https://get.popcorntime.io/build/$ARCHIVE
 wget -c $URLBASE
 
-ARCH=Linux64
+ARCH=Linux-64
 ARCHIVE=Popcorn-Time-${VERSION}-${ARCH}.tar.xz
 URLBASE=https://get.popcorntime.io/build/$ARCHIVE
 wget -c $URLBASE
