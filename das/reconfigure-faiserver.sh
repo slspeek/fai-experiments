02db2eb56109624dd73ba3a72e9f640df1645eb1
Steven L. Speek <slspeek@gmail.com>
2015-08-17 13:41:15 +0200

    reconfigure faiserver started

---
+++ das/reconfigure-faiserver.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+IPADDR=$1 
+NETWORK=$2
+OLDIPADDR=$3
+OLDNETWORK=$4
+
+SEDCMD="sed -i -e \"s/${OLDIPADDR}/$IPADDR/g;s/$OLDNETWORK/$NETWORK/g\""
+
+echo CMD: $SEDCMD
+
+$SEDCMD /usr/local/bin/pxe{,-i386}.sh /etc/exports /etc/dhcp/dhcpd.conf /etc/network/interfaces /etc/hosts /etc/fai/apt/sources.list 
+
+
