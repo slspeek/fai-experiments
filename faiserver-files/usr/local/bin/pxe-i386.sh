43cd17ebe32590152ba9bc5f09b93b4c46aa393f
Steven L. Speek <slspeek@gmail.com>
2016-02-18 20:32:23 +0100

    faiserver-files removed

--- faiserver-files/usr/local/bin/pxe-i386.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-HOST=$1
-
-PXECONFIG=$(fai-chboot -u nfs://10.0.50.2/srv/fai/config -s 686-pae -IFv $HOST 2>&1|tail -1|cut -d' ' -f3)
-#sed -i -e 's%/srv/fai/nfsroot%10.0.40.2:/srv/fai-i386/nfsroot:vers=3%' $PXECONFIG
